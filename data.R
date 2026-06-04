f6a2c73008cdd04f2f1ddc076c67167c54106f4b
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-06-04 10:51:17 +0300

    feat: add data.R
    This function updates data by sourcing R scripts from the 'data-raw' directory and committing changes to Git.

---
+++ data.R
@@ -0,0 +1,20 @@
+update_data <- function() {
+  r_script_paths <- dir("data-raw", pattern = "[.][rR]$", full.names = TRUE)
+  r_script_paths <- r_script_paths[
+    !grepl("^[0-9]+_[0-9]+", r_script_paths)
+  ]
+  
+  s1 <- system2("git", "status", stdout = TRUE)
+  stopifnot(
+    "nothing to commit, working tree clean" %in% s1
+  )
+
+  lapply(r_script_paths, function(r_script_path) {
+    source(r_script_path)
+    system2("git", c("add", "-A"))
+    msg <- sprintf("\"feat: run %s\"", substr(r_script_path, 1, 30))
+    system2("git", c("commit", "-m", msg))
+  })
+  invisible(NULL)
+}
+update_data()
