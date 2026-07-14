21ce41191db6496885de2e38c86b1c0e4891f2a2
Joonas Miettinen <joonas.miettinen@cancer.fi>
2026-07-14 13:08:21 +0300

    fix: cran_06_post_accept.R

--- cran_06_post_accept.R
+++ cran_06_post_accept.R
@@ -12,6 +12,10 @@ du$git_exe_cmd(c("pull", "--tags", "--force"))
 du$git_exe_cmd(c("checkout", git_ref))
 du$git_exe_cmd(c("tag", sprintf("v%s", cran_version)))
 du$git_exe_cmd(c("checkout", "master"))
+tryCatch(
+  du$git_exe_cmd(c("tag", "-d", "release")),
+  error = function(e) e
+)
 du$git_exe_cmd(c("tag", "release"))
 du$git_exe_cmd(c("push", "--tags", "--force"))
 
